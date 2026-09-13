1e65d3c5f787e11aae7078e3c37e36b0d7f3faf9
Nick Garcia <nicholasgarcia2003@protonmail.com>
2026-09-12 22:52:23 -0600

    feat: add service for ranking engine
    This service will sift through pages in the database and assign them
    scores that will affect their search rankings.
    
    I wanted a strong general-purpose language with access to lots of
    third-party NLP and ML libraries. Narrowing down to Python or Java, we
    chose Java for its execution speed, as most of the NLP-dependent heavy
    lifting will be done by the indexer service.

---
+++ ranking_engine/src/main.java
@@ -0,0 +1,7 @@
+package ranking_engine.src;
+
+class RankingEngine {
+    public static void main(String[] args) {
+        System.out.println("I'm the ranking engine! I judge webpages' importance!");
+    }
+}
