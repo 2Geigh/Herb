387d8174969c956947b6757d76258301e4be3978
Nick Garcia <nicholasgarcia2003@protonmail.com>
2026-09-13 15:36:24 -0600

    feat: create Indexer's API endpoints and concurrent execution
    The indexer can both listen for incoming http requests *and* work on
    indexing at the same time!

--- indexer/main.py
+++ indexer/main.py
@@ -1,11 +1,47 @@
-import psycopg2
-from typing import Optional
 import db
+from fastapi import FastAPI
+import uvicorn
+import asyncio
 
-def main():
-    conn = db.connect()
+local_queue: list[str] = []
 
-    db.disconnect(conn)
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    print("GET / recieved")
+    return {"message": "I'm the indexer!"}
+
+@app.put("/queue")
+def enqueue():
+    print("PUT /queue RECEIVED")
+    return {"message": "PUT request recieved"}
+
+async def index() -> RuntimeError:
+    while True:
+        print("Still indexing web pages...", flush=True)
+        await asyncio.sleep(1.25)
+
+# def main():
+#     conn = db.connect()
+
+#     db.disconnect(conn)
+
+async def main() -> None:
+    indexing_task = asyncio.create_task(index())
+
+    config = uvicorn.Config(
+        app,
+        host="0.0.0.0",
+        port=3001,
+    )
+    server = uvicorn.Server(config)
+
+    try:
+        await server.serve()
+    finally:
+        indexing_task.cancel()
+        await asyncio.gather(indexing_task, return_exceptions=True)
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
